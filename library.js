b2903ae557f3589b83bb43551354667241c6fc9c
Donquellso <kamil080801@gmail.com>
2024-06-11 23:43:35 +0200

    Fix

--- library.js
+++ library.js
@@ -1,167 +1,110 @@
 const library = [
-    {
-        name: 'Duma i uprzedzenie',
-        author: 'Jane Austen',
-        pages: 350,
-        isRead: true,
-<<<<<<< HEAD
-        rate: 4
-=======
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-    },
-    {
-        name: '1984',
-        author: 'George Orwell',
-        pages: 280,
-        isRead: false,
-<<<<<<< HEAD
-        rate: 2
-=======
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-    },
-    {
-        name: 'Mistrz i Małgorzata',
-        author: 'Michaił Bułhakow',
-        pages: 450,
-        isRead: true,
-<<<<<<< HEAD
-        rate: 4
-=======
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-    },
-    {
-        name: 'Zbrodnia i kara',
-        author: 'Fiodor Dostojewski',
-        pages: 600,
-        isRead: false,
-<<<<<<< HEAD
-        rate: 3
-=======
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-    },
-    {
-        name: 'Ania z Zielonego Wzgórza',
-        author: 'Lucy Maud Montgomery',
-        pages: 320,
-        isRead: true,
-<<<<<<< HEAD
-        rate: 1
-=======
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-    },
-    {
-        name: 'Dzieci z Bullerbyn',
-        author: 'Astrid Lindgren',
-        pages: 150,
-        isRead: true,
-<<<<<<< HEAD
-        rate: 0
-=======
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-    },
-    {
-        name: 'Dżuma',
-        author: 'Albert Camus',
-        pages: 200,
-        isRead: false,
-<<<<<<< HEAD
-        rate: 2
-=======
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-    },
-    {
-        name: 'Metro 2033',
-        author: 'Dmitrij Głuchowski',
-        pages: 400,
-        isRead: true,
-<<<<<<< HEAD
-        rate: 4
-=======
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-    },
-    {
-        name: 'Mały Książę',
-        author: 'Antoine de Saint-Exupéry',
-        pages: 100,
-        isRead: true,
-<<<<<<< HEAD
-        rate: 3
-=======
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-    },
-    {
-        name: 'Dziennik Bridget Jones',
-        author: 'Helen Fielding',
-        pages: 320,
-        isRead: false,
-<<<<<<< HEAD
-        rate: 1
-=======
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-    },
-    {
-        name: 'Harry Potter',
-        author: 'J.K. Rowling',
-        pages: 300,
-        isRead: true,
-<<<<<<< HEAD
-        rate: 4
-    },
-    {
-=======
+        {
+            name: 'Duma i uprzedzenie',
+            author: 'Jane Austen',
+            pages: 350,
+            isRead: true,
+            rate: 4
         },
         {
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-        name: 'Wiedźmin',
-        author: 'Andrzej Sapkowski',
-        pages: 400,
-        isRead: false,
-<<<<<<< HEAD
-        rate: 2
-    },
-    {
-=======
+            name: '1984',
+            author: 'George Orwell',
+            pages: 280,
+            isRead: false,
+            rate: 2
         },
         {
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-        name: 'Hobbit',
-        author: 'J.R.R. Tolkien',
-        pages: 500,
-        isRead: true,
-<<<<<<< HEAD
-        rate: 0
-    },
-    {
-=======
+            name: 'Mistrz i Małgorzata',
+            author: 'Michaił Bułhakow',
+            pages: 450,
+            isRead: true,
+            rate: 4
         },
         {
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-        name: 'W pustyni i w puszczy',
-        author: 'Henry Sienkiewicz',
-        pages: 200,
-        isRead: false,
-<<<<<<< HEAD
-        rate: 3
-    },
-    {
-=======
+            name: 'Zbrodnia i kara',
+            author: 'Fiodor Dostojewski',
+            pages: 600,
+            isRead: false,
+            rate: 3
         },
         {
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
-        name: 'Magiczne drzewo',
-        author: 'Andrzej Maleszka',
-        pages: 100,
-        isRead: true,
-<<<<<<< HEAD
-        rate: 1
-    }
-];
-
-export default library;
-=======
+            name: 'Ania z Zielonego Wzgórza', // Added missing name property
+            author: 'Lucy Maud Montgomery',
+            pages: 320,
+            isRead: true,
+            rate: 1
+        },
+        {
+            name: 'Dzieci z Bullerbyn',
+            author: 'Astrid Lindgren',
+            pages: 150,
+            isRead: true,
+            rate: 0
+        },
+        {
+            name: 'Dżuma',
+            author: 'Albert Camus',
+            pages: 200,
+            isRead: false,
+            rate: 2
+        },
+        {
+            name: 'Metro 2033',
+            author: 'Dmitrij Głuchowski',
+            pages: 400,
+            isRead: true,
+            rate: 4
+        },
+        {
+            name: 'Mały Książę',
+            author: 'Antoine de Saint-Exupéry',
+            pages: 100,
+            isRead: true,
+            rate: 3
+        },
+        {
+            name: 'Dziennik Bridget Jones',
+            author: 'Helen Fielding',
+            pages: 320,
+            isRead: false,
+            rate: 1
+        },
+        {
+            name: 'Harry Potter',
+            author: 'J.K. Rowling',
+            pages: 300,
+            isRead: true,
+            rate: 4
+        },
+        {
+            name: 'Wiedźmin',
+            author: 'Andrzej Sapkowski',
+            pages: 400,
+            isRead: false,
+            rate: 2
+        },
+        {
+            name: 'Hobbit',
+            author: 'J.R.R. Tolkien',
+            pages: 500,
+            isRead: true,
+            rate: 0
+        },
+        {
+            name: 'W pustyni i w puszczy',
+            author: 'Henryk Sienkiewicz',
+            pages: 200,
+            isRead: false,
+            rate: 3
+        },
+        {
+            name: 'Magiczne drzewo',
+            author: 'Andrzej Maleszka',
+            pages: 100,
+            isRead: true,
+            rate: 1
         }
-];
-
-
+    ];
+    
     export default library;
->>>>>>> 27d4034716d9ece05e64e9b93fd2deaa388d737b
+    
